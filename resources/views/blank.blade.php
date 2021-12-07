27005ae44afe42509bb2e2ccf389890be39d5043
Anderson Martins <tuco.anderson@gmail.com>
2021-12-06 23:04:23 -0300

    customização da barra de navegação padrão

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Products')
+
+@section('content')
+
+
+<div class="card" style="margin-top: 10px; height: 72vh;">
+    <div class="card-body">
+        <h1>Bem vindo aos produtos</h1>
+    </div>
+</div>
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,9 @@
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://unpkg.com/feather-icons"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -19,14 +22,7 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <img 
-                        src="https://www.imagenspng.com.br/wp-content/uploads/2020/10/among-us-icon-png-02.png" 
-                        alt="icon" 
-                        class="bi me-2" 
-                        width="40" 
-                        height="40" 
-                        role="img" 
-                        aria-label="Bootstrap">
+                    <img src="https://www.imagenspng.com.br/wp-content/uploads/2020/10/among-us-icon-png-02.png" alt="icon" class="bi me-2" width="40" height="40" role="img" aria-label="Bootstrap">
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -36,16 +32,35 @@
                     <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">About</a></li>
                 </ul>
+                @if (Route::has('login') && Auth::check())
 
-                <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2">Login</button>
-                    <button type="button" class="btn btn-warning">Sign-up</button>
+                <div class="dropdown" >
+                    <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <li>
+                             <input class="dropdown-item" type="submit" value="Logout">
+
+                            </li>
+                        </form>
+                    </ul>
                 </div>
+
+                @elseif (Route::has('login') && !Auth::check())
+                <div class="text-end">
+                    <a href="{{ url('/login') }}"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
+                    <a href="{{ url('/register') }}"><button type="button" class="btn btn-warning">Sign-up</button></a>
+                </div>
+                @endif
+
             </div>
         </div>
     </header>
 
-    <div class="container" >
+    <div class="container">
         @yield('content')
     </div>
 
