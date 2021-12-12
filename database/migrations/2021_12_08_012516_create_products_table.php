2a9a7df09954874a8f47423103c005d8b244956a
Anderson Martins <tuco.anderson@gmail.com>
2021-12-12 13:59:33 -0300

    permitir descrição nula

--- database/migrations/2021_12_08_012516_create_products_table.php
+++ database/migrations/2021_12_08_012516_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id');
             $table->string('partnumber');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
