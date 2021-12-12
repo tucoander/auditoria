94b26737509b2b27b4740637120e319ee95b88e4
Anderson Martins <tuco.anderson@gmail.com>
2021-12-12 15:09:15 -0300

    criando controller para as caixas

---
+++ app/Http/Controllers/CartonController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CartonModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CartonController extends Controller
+{
+    public function index()
+    {
+        return view('cartons/create');
+    }
+
+    public function store(Request $request){
+        $carton = new CartonModel();
+
+        $cartonAlreadyExists = $carton::where('shipping_hu', $request->shipping_hu)->count();
+
+        if ($cartonAlreadyExists < 1) {
+            $carton->id = Str::uuid();
+            $carton->shipping_hu = $request->shipping_hu;
+            $carton->document = $request->document;
+
+            $carton->save();
+
+            return redirect('/cartons')->with('msg', 'Caixa cadastrada');
+        } else {
+            return redirect('/cartons')->with('msg', 'Caixa já possui cadastro  , favor revisar')->with('status', 1);
+        }
+    }
+}
